5db7f19e3a9d1d6d8dcf1f043407f7db7adb5dc3
shuvo1710 <mdshovo102002@gmail.com>
2023-02-26 17:40:06 +0600

    added

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
-const cors = require('cors'); 
-const { query, query } = require('express');
+const cors = require('cors');
 require("dotenv").config()
 
 
@@ -11,61 +10,55 @@ app.use(cors())
 app.use(express.json())
 
 
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.1mrcu36.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // console.log(uri);
 
-const run =async()=>{
-    
-    try{
-        const allProductCollection=client.db('mss-mart').collection('allProduct')
-        const womanProductCollection = client.db ('mss-mart').collection('womanCollection')
-        const manProductCollection = client.db ('mss-mart').collection('manCollection')
+const run = async () => {
+
+    try {
+        const allProductCollection = client.db('mss-mart').collection('allProduct')
+        const womanProductCollection = client.db('mss-mart').collection('womanCollection')
+        const manProductCollection = client.db('mss-mart').collection('manCollection')
         const kidsProductCollection = client.db('mss-mart').collection('kidCollection')
         const recommendationProduct = client.db('mss-mart').collection('recommendationCollection')
+        const bestSealProductsCollection = client.db('mss-mart').collection('bestSealCollection')
 
 
-
-
-
-        app.get('/allProduct', async(req,res)=>{
-            const query = {}
+        app.get('/allProduct', async (req, res) => {
+            const productType = req.query.productType
+            const query = { productType: productType }
             const allProduct = await allProductCollection.find(query).toArray()
             res.send(allProduct)
         })
 
-        app.get('/woman', async(req,res)=>{
-            const query = {}
-            const woman = await womanProductCollection.find(query).toArray()
-            res.send(woman)
+        app.get('/allProducts', async (req, res) => {
+            const category = req.query.category;
+            const query = { category: category }
+            const allProducts = await allProductCollection.find(query).toArray();
+            res.send(allProducts)
         })
-        app.get('/man', async(req,res)=>{
-            const query = {}
-            const man = await manProductCollection.find(query).toArray()
-            res.send(man)
-        })
-        app.get('/man/:id', async(req,res)=>{
+
+
+        app.get('/allProduct/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)}
-            const result = await manProductCollection.findOne(query)
-            res.send(result)
+            const query = { _id: new ObjectId(id) }
+            const findProduct = await allProductCollection.findOne(query)
+            res.send(findProduct)
         })
-
-
-        app.get('/kids', async(req,res)=>{
+        app.get('/recommendation', async (req, res) => {
             const query = {}
-            const man = await kidsProductCollection.find(query).toArray()
-            res.send(man)
-        })
-        app.get('/recommendation', async(req,res)=>{
-            const query ={}
-            const recommendation= await recommendationProduct.find(query).limit(15).toArray()
+            const recommendation = await recommendationProduct.find(query).limit(15).toArray()
             res.send(recommendation)
+        })
+        app.get('/bestSeal', async (req, res) => {
+            const query = {}
+            const BestSeal = await bestSealProductsCollection.find(query).toArray()
+            res.send(BestSeal)
         })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -74,9 +67,9 @@ run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
